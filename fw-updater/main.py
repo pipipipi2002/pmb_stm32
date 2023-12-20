e54b11afd24ad00812f0e60ec4e05f221182c6f3
Marvin Pranajaya <marvinpofficial@gmail.com>
2023-12-20 23:30:49 +0800

    cleanup fw uploader python script for lab testing

--- fw-updater/main.py
+++ fw-updater/main.py
@@ -9,26 +9,25 @@ def send_one(opt):
     # see https://python-can.readthedocs.io/en/stable/configuration.html
     with can.Bus(interface='canine', bitrate=1000000) as bus:
 
-        # if opt == 1:
-        #     msg = can.Message(
-        #         arbitration_id=40, data=[0xBB], is_extended_id=False
-        #     )
-        # elif opt == 2:
-        #     msg = can.Message(
-        #         arbitration_id=40, data=[0xAA], is_extended_id=False
-        #     )
-        data_bank = [0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA]
-        for i in data_bank:
+        if opt == 2: # Echo
             msg = can.Message(
-                arbitration_id=40, data=[i], is_extended_id=False
+                arbitration_id=40, data=[0xBB], is_extended_id=False
             )
+        elif opt == 1: # Jump to Main
+            msg = can.Message(
+                arbitration_id=40, data=[0xAA], is_extended_id=False
+            )
+        # data_bank = [0xAA]
+        # for i in data_bank:
+        #     msg = can.Message(
+        #         arbitration_id=40, data=[i], is_extended_id=False
+        #     )
 
-            try:
-                print(hex(i))
-                bus.send(msg)
-                print(f"Message sent on {bus.channel_info}")
-            except can.CanError:
-                print("Message NOT sent")
+        try:
+            bus.send(msg)
+            print(f"Message sent on {bus.channel_info}")
+        except can.CanError:
+            print("Message NOT sent")
 
 def start_rx():
     with can.Bus(interface='canine', bitrate=1000000) as bus:
@@ -43,7 +42,7 @@ def start_rx():
 
 if __name__ == "__main__":
     while True:
-        rx = input("send?")
+        rx = input("Send CAN message \n\t1: Enter APP \n\t2: Echo\n>>")
         if (int(rx) == 1):
             send_one(1)  
         elif (int(rx) == 2):
